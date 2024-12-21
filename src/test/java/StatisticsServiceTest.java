a17951040c12d5bdd5944fc9ee96014950bb105d
NestJul <nestjul@yandex.ru>
2024-12-22 00:22:17 +0700

    add test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -4,9 +4,17 @@ public class StatisticsServiceTest {
     @Test
     void findMax() {
         ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
     }
+ @Test
+void currentMax() {
+    ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+    long[] income = {0};
+    long expected = 0;
+    long actual = service.currentMax(income);
+    Assertions.assertEquals(expected, actual);
+}
 }
